2cf6b8fcc567a50e398e122e9d0c189c3cbe817d
smart <zeynel.sevgili@gmail.com>
2018-08-03 16:28:44 +0300

    UISearchBar1 (Query yapmıyor.) Bunun haricinde herşey tamam

--- TodoList/Controllers/TodoListViewController.swift
+++ TodoList/Controllers/TodoListViewController.swift
@@ -1,12 +1,8 @@
-//
-//  ViewController.swift
-//  TodoList
-//
-//  Created by Demo on 30.07.2018.
-//  Copyright © 2018 RN. All rights reserved.
-//
+
 
 import UIKit
+// bu satırı eklemeyi unutma
+import CoreData
 
 // main story de default gelen UIView i sildik. yerine sağ alttan tableViewController sürükledik. şimdi bunu bu sınıf ile bağlamak için UIViewController(default) sınıfını silip yerine "UITableViewController" ekliyoruz. sonra da sağ taraftan
 // "ViewController" yazan dosyanın ismini "TodoListViewController" koyuyoruz(keyfi). sonrasıda aşağıdaki class ismini de anyı isimle değiştiriyoruz. En sonda da inspector kısmından class ismini değiştiriyoruz.
@@ -24,26 +20,20 @@ class TodoListViewController: UITableViewController {
     
     
     // filemanager ile data file path oluşturuyoruz. array olduğu için ilk elemanı istiyoruz. first den sonraki kısım Item adlı plist dosyası oluşturmamız içindir. Global constant olarak tanıtıyoruz.
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+  
+    
+    // aşağıdaki satırla UIApplication sınıfına giriyoruz. shared singleton objesini alıyoruz. persistentContainer giriyoruz ve viewContext i yakalıyoruz.
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
 // aşağıdaki kodlara artık lüzum görmüyoruz. loadItems() metoduyla verileri plistten decode edip  array türüne dönüştüreceğiz
     override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//
-//        let newItem = Item()
-//        newItem.title = "Find Zeynel"
-//        itemArray.append(newItem)
-//
-//        let newItem2 = Item()
-//        newItem2.title = "Find Asude"
-//        itemArray.append(newItem2)
-//
-//        let newItem3 = Item()
-//        newItem3.title = "Find Muhammed"
-//        itemArray.append(newItem3)
+     super.viewDidLoad()
+
     
+        // database dosyamızın hangi dosyada saklı olduğunu öğrenmek için yazıyoruz bu kodu
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         loadItems()
         
         // retrieve data from a registered plist(local de "self.defaults.set(self.itemArray, forKey: "TodoListArray")"
@@ -83,25 +73,38 @@ class TodoListViewController: UITableViewController {
         // burada seçilen row un accessory kısmını checkmark yapıyoruz. eğer checkmark ise none ile uncheck yaptırıyoruz.
         // toggle ve untoggle yaptırıyoruz burada.
         // dikkat! operatörü aynen aşağıda kullanıldığı gibi kullan yoksa hata veriyor.
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-       // reload metodu saveItems() metodunda kullanıldığı için tekrardan yazmaya gerek yok.
-        saveItems()
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        // herhangi bir satıra basıldığında satırın silinmesini istiyorsak sıralamanın aşağıdaki gibi olması gerekiyor. aksi takdirde hata veriyor. ilk başta context den sonra itemArraydan silinmesi gerekiyor. aksi takdirde itemAray objesi kullanıldığı için index hatası veriyor.
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+      
+       // reload metodu saveItems() metodunda kullanıldığı için tekrardan yazmaya gerek yok.
+        // yaptığımız değişikliklerden sonra veriyi permenant yapmak için bu metodu kullanmamız gerekiyor. 
+        
+        saveItems()
+      
+        
+        //tableView.deselectRow(at: indexPath, animated: true)
     }
     
     //Mark - Add new items
-    
+
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Yeni Bir Yapılacak Ekle", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Yeni Ekle", style: .default) { (action) in
         
-        // alert kısmından gelen ve global değişkene atanan değer array list e ekleniyor.
-            let newItem = Item()
+            
+            
+            
+            // butona basar basmaz bir Item objesi oluşturuluyor. Bu Item objesi CoreModel attribute ları oluşturulurken otomatik oluşturulmuştu. Bu sınıflar localde library de görüntülenebilir. videolarda mevcut
+            let newItem = Item(context: self.context)
+            // şimdi her bir attribute a ulaşabiliyoruz ve bunları dolduruyoruz.
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
-            // yeni bir şey eklediğimizde plist e ekleneceği için aşağıdaki metodu çağırıyoruz.
+            // bu attribute lar doldurulduktan sonra kalıcı olarak saklamak için save ediyoruz. aşağıdaki metodun içeriğine bak
             self.saveItems()
             
         }
@@ -116,43 +119,61 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    // done kısmının custom plist e kaydedilmesini de burada yapıyoruz. clouser olmadığı için self komutunu siliyoruz.
-    // burada verileri arraydan plist dosyasına aktarırken "encode" yapıyoruz.
+   
     func saveItems() {
-        // verileri append ettikten sonra saklamayı sağlıyoruz. aşağıdaki satır User defaults ile ilgili olduğu için programın çökmesine neden oluyor. bunu uncomment yapıp custom veri tipi için aşağıdaki satırları yazıyoruz.
-        // self.defaults.set(self.itemArray, forKey: "TodoListArray")
-        let encoder = PropertyListEncoder()
-        // encode edeceğimiz obje itemArray
+       
         do {
-            // ilk satırda encode ediyoruz array ı, ikinci satırda da path ile belirtilen dosyaya yazıyoruz.
-            let data = try encoder.encode(itemArray)
-            try data.write(to: self.dataFilePath!)
+            // CRUD işlemlerinden sonra verilerden memnun isek save() metodu ile verileri persistent/Permanent yapıyoruz
+           try context.save()
         }catch{
-            print("error encoding item array, \(error)")
+            print("Error Saving Context \(error)")
         }
         // bu satır olmadan table a yansımıyor yani görünmüyor. append yaptıktan sonra mutlaka table view e reload yapmak gereklidir.
+        // crud yapıldıktan sonra da bu metodun çağrılması gerekiyor ki table tekrardan yüklensin
         self.tableView.reloadData()
         
     }
     // burada yukarıdakine farklı olarak verileri plist ten array a "decode" ediyoruz. yani her iki durumda da bir dönüşüm söz konusu...
-    func loadItems(){
+    
+    // request internal item. with request internal item.
+    // loadItems() ile parametresiz çağrıldığında default olarak eşitliğin sağ tarafındaki kısım icra edilir yani request değişkenine atanır. loadItems(with: request) parametre ile çağrıldığında eşitliğin sol yanı icra ediliyor yani bir nevi sağ taraf yok sayılıyor.
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
         
-        if let data = try? Data(contentsOf: dataFilePath!){
-            
-            let decoder = PropertyListDecoder()
-            // plist ten decode yaparaken aşağıdaki yapıyı kullanıyoruz. ilk parametre type: Item
-            do{
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("erorr by the time decoding \(error)")
-            }
+        // context fetch request yapıyor.
+        do{
+        itemArray = try context.fetch(request)
+        } catch {
+            print("error by the time of fetching \(error)")
         }
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK: - Search Bar Metodları
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        // contexten bilgi almak için fetch request yapıyoruz.
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // Aşağıdaki nasıl sorgu yapılacağını belirtiyor.
+        // Database den title attribute a bakılacak. %@(searchBar.text!) argümanının içerecek(CONTAINS). [cd] komutu case insensitive yapıyor aramayı. [cd] komutunu yerleştirmeksek case sensitive olacak.
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        
+       
+        
+        // Aşağıdaki satır database den aldığımız satırın nasıl sıralanacağını belirtiyor.
+        // Belirtildiği gibi alfabetik sıraya göre sıralama yapacak.
+         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        // request i parametre olarak gönderiyoruz. tableReload u aşağıya yazmamıza gerek yok.
+        loadItems(with: request)
+   
         
     }
     
-    
-    
-    
-
 }
 
